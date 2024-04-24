8327218bf18e4a0b1869d88e3363e59985c1300f
Julio Cesar Almeida Dourado <88950795+typejulio@users.noreply.github.com>
2024-04-24 00:39:39 -0300

    Upload do site de portfolio

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-btn').addEventListener('click', function() {
+    var optionsMenu = document.getElementById('options-menu');
+    optionsMenu.classList.toggle('show-menu');
+});

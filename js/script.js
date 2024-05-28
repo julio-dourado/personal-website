53672da5dae3a6e06fcf17d87229b0d52026c43d
Julio Cesar <juliodourado419@gmail.com>
2024-05-27 22:17:39 -0300

    download cv

--- js/script.js
+++ js/script.js
@@ -1,22 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const menuBtn = document.getElementById('menu-btn');
     const optionMenu = document.getElementById('option-menu');
 
-    menuBtn.addEventListener('click', function() {
+    menuBtn.addEventListener('click', function () {
         if (optionMenu.style.display === 'none' || optionMenu.style.display === '') {
             optionMenu.style.display = 'block';
         } else {
             optionMenu.style.display = 'none';
         }
     });
-});
-
-
-document.getElementById("download-cv-btn").addEventListener("click", function() {
-    var link = document.createElement("a");
-    link.href = "./../cv/cv.pdf";
-    link.download = "julio-dourado-cv.pdf";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
 });
